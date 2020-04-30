f45077a838e785c5f6a9a936662b6eed5dde1177
Nitay Kessner <nitayke@gmail.com>
2020-04-30 22:33:34 +0300

    bind and listen

--- Trivia Project/Communicator.cpp
+++ Trivia Project/Communicator.cpp
@@ -1,1 +1,17 @@
 #include "Communicator.h"
+
+void Communicator::bindAndListen()
+{
+    struct sockaddr_in sa = { 0 };
+	sa.sin_port = htons(PORT);
+	sa.sin_family = AF_INET;
+	sa.sin_addr.s_addr = 0;
+	// again stepping out to the global namespace
+	if (::bind(_socket, (struct sockaddr*)&sa, sizeof(sa)) == SOCKET_ERROR)
+		throw std::exception(__FUNCTION__ " - bind");
+	printf("binded\n");
+
+	if (::listen(_socket, SOMAXCONN) == SOCKET_ERROR)
+		throw std::exception(__FUNCTION__ " - listen");
+	printf("listening...\n");
+}
