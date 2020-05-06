70ef70d3a8adc346d1396fa6cbde2e9f441baf08
Nitay Kessner <nitayke@gmail.com>
2020-05-06 18:22:37 +0300

    new structs and methods in the interface

--- Trivia Project/LoginRequestHandler.cpp
+++ Trivia Project/LoginRequestHandler.cpp
@@ -1,1 +1,11 @@
 #include "LoginRequestHandler.h"
+
+bool LoginRequestHandler::isRequestRelevant(RequestInfo requestinfo)
+{
+	return false;
+}
+
+RequestResult LoginRequestHandler::handleRequest(RequestInfo requestinfo)
+{
+	return RequestResult();
+}
