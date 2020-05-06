9ce7ff0ebf5e425358e13e2f5d54075002781a71
Nitay Kessner <nitayke@gmail.com>
2020-05-06 13:24:35 +0300

    input check in socket python

--- client.py
+++ client.py
@@ -3,13 +3,18 @@ import socket
 SERVER_IP = "127.0.0.1"
 SERVER_PORT = 2222
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+try:
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    print("Socket successfully created")
+except socket.error as err:
+    print("socket creation failed with error %s") % err
+
 server_address = (SERVER_IP, SERVER_PORT)
 sock.connect(server_address)
 
 server_msg = sock.recv(5)
-server_msg = server_msg.decode()
-print(server_msg)
+print(server_msg.decode())
+
 if server_msg == "Hello":
     client_msg = "Hello"
     sock.sendall(client_msg.encode())
